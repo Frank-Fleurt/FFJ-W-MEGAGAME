f9d5e9b9115b9eefe4ab91aaefd5db6b4dda82a7
Florian Pellier <92161528+peflor@users.noreply.github.com>
2022-10-13 10:47:05 +0200

    wip

--- Classes/Monstres/Monstre.js
+++ Classes/Monstres/Monstre.js
@@ -1,7 +1,9 @@
 export class Monstres {
-    constructor(name = "GobeHipo", hp = 500, dammage = 50){
+    constructor(name = "GobeHipo", hp = 100, dammage = 50){
         this.name = name;
         this.hp = hp;
         this.dammage = dammage;
     }
+
+    attaquer()
 }
