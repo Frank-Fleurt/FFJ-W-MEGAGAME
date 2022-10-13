262f209325584e29f790d03dee01cb87b8a57970
Florian Pellier <92161528+peflor@users.noreply.github.com>
2022-10-13 10:15:11 +0200

    wip

---
+++ Classes/Monstres/Monstre.js
@@ -0,0 +1,10 @@
+export class Monstres {
+    constructor(type, nom, sante, faiblesse, degat){
+        this.type = type;
+        this.nom = nom;
+        this.sante = sante;
+        this.faiblesse = faiblesse;
+        this.degat = degat;
+    }
+
+}
