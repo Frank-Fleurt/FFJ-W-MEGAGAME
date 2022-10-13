ad4951f46582294d5937b7cc66c380f2aa8822d2
Florian Pellier <92161528+peflor@users.noreply.github.com>
2022-10-13 11:19:12 +0200

    wip

--- Classes/Monstres/Monstre.js
+++ Classes/Monstres/Monstre.js
@@ -1,10 +1,12 @@
 export class Monstres {
-    constructor(type, nom, sante, faiblesse, degat){
-        this.type = type;
-        this.nom = nom;
-        this.sante = sante;
-        this.faiblesse = faiblesse;
-        this.degat = degat;
+    constructor(name = "GobeHipo", hp = 100, dammage = 50){
+        this.name = name;
+        this.hp = hp;
+        this.dammage = dammage;
     }
 
+    attaquer(ennemi){
+        ennemi.hp -= this.dammage
+        console.log(`GobeHipo lance son attaque Pourrie et inflige ${this.dammage} points de dommage a ${ennemi}`)
+    }
 }
