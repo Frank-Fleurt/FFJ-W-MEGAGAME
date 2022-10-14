561d2b30610cbfcff0ffa34a72a2128140748548
Frank <frank.fleurt@hotmail.com>
2022-10-14 11:38:57 +0200

    modif rounds

--- Classes/Combat/Weapon.js
+++ Classes/Combat/Weapon.js
@@ -1,4 +1,4 @@
-class Weapon {
+export class Weapon {
 	specialAbility = 1;
 	constructor(name, cost, effect) {
 		this.name = name;
@@ -6,13 +6,5 @@ class Weapon {
 		this.affect = effect.affect;
 		this.power = effect.power;
 		this.specialPower = this.power * 2
-	}
-
-	display() {
-		return `Vous avez utilisé l'arme ${this.name} qui vous as couté ${this.cost} point d'endurance`
-	}
-
-	displaySpecial() {
-		return `Vous vous avez utiliser l'attaque spéciale de l'arme augmentant les effet par 2 avec la même utilisation d'énergie`
 	}
 }
